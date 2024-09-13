70144d973c8ccc5959d6a14a5a9e2392af7003c3
InputYourNamePlz <118294940+InputYourNamePlz@users.noreply.github.com>
2024-09-13 23:13:25 +0900

    ebimu's x/y axis setting is fxxk

--- ebimu_driver/ebimu_driver.py
+++ ebimu_driver/ebimu_driver.py
@@ -9,16 +9,24 @@ except:
     pip.main(['install', pyserial])
     import serial
 import math
+import threading
 
 class EbimuDriver(Node):
     def __init__(self):
         super().__init__('ebimu_driver')
         self.publisher = self.create_publisher(Imu, 'imu', 10)
         self.serial_port = serial.Serial('/dev/ttyIMU', 115200, timeout=1)  # 시리얼포트는 tty 고정해서 쓰면 좋아용
-        self.timer = self.create_timer(0.01, self.timer_callback)  # 100Hz, 필요에 따라 조정
+        #self.timer = self.create_timer(0.01, self.timer_callback)  # 100Hz, 필요에 따라 조정
         self.timer2 = self.create_timer(0.1, self.timer2_callback)
         #self.serial_port.write("<sof2>".encode()) # quaternion 모드로 설정
         #self.serial_port.write("<sem0>".encode()) # 자기장 센서 끄고 싶을 때
+
+
+        self.thread = threading.Thread(target=self.serial_read_thread)
+        self.thread.daemon = True
+        self.thread.start()
+
+        
 
         self.qz=0.0
         self.qy=0.0
@@ -26,34 +34,30 @@ class EbimuDriver(Node):
         self.qw=0.0
         self.gx=0.0
         self.gy=0.0
+        self.gz=0.0
         self.ax=0.0
         self.ay=0.0
         self.az=0.0
         
-        self.is_upside_down=True
+        self.is_upside_down=False
+        
 
 
     
-    def timer2_callback(self): 
-        qx = self.qx
-        gx = self.gx
-        ax = self.ax       
-        if(self.is_upside_down==True):
-            qw=-self.qw
-            qy=-self.qy
-            qz=-self.qz
-            gy=-self.gy
-            gz=-self.gz
-            ay=-self.ay
-            az=-self.az
-        else:
-            qw=self.qw
-            qy=self.qy
-            qz=self.qz
-            gy=self.gy
-            gz=self.gz
-            ay=self.ay
-            az=self.az
+    def timer2_callback(self):  
+
+        qw=self.qw
+        qx=-self.qy
+        qy=self.qx
+        qz=self.qz
+
+        gx=self.gy
+        gy=self.gx
+        gz=self.gz
+
+        ax=self.ay  
+        ay=self.ax
+        az=self.az
 
             
         imu_msg = Imu()
@@ -68,6 +72,22 @@ class EbimuDriver(Node):
         print(f"IMU data published: {qx, qy, qz, qw, gx, gy, gz, ax, ay, az}")
 
     
+    def serial_read_thread(self):
+        while rclpy.ok():
+            try:
+                if self.serial_port.in_waiting > 0:
+                    line = self.serial_port.readline().decode('utf-8').strip()
+
+                if line.startswith('*'):
+                    data = line[1:].split(',')
+                    if len(data) == 10:  # 예상되는 데이터 개수
+                        self.qz, self.qy, self.qx, self.qw, self.gx, self.gy, self.gz, self.ax, self.ay, self.az = map(float, data)
+                else:
+                    print(f"data length is {len(data)}")
+            except Exception as e:
+                self.get_logger().error(f'Error reading from serial port: {str(e)}')
+
+
     def timer_callback(self):
         try:
             line = self.serial_port.readline().decode('utf-8').strip()
